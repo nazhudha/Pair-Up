d2849d5c85cfa040a277f625ee8c91ed4b56b0eb
Saif-ul Hussain <saif-ulhussain@saif-uls-air.home>
2022-08-01 16:24:40 +0100

    Created Test Case to Save and Find User Data and Created MongoDB Helper

--- backend/spec/model/user.spec.js
+++ backend/spec/model/user.spec.js
@@ -1,5 +1,7 @@
 const User = require('../../models/user');
 
+require('../mongodb_helper');
+
 describe('User model', () => {
   it('create an instance of the User Model Schema', () => {
     const user = new User({
@@ -10,5 +12,25 @@ describe('User model', () => {
     expect(user.username).toEqual("Test User");
     expect(user.email).toEqual("example@example.com");
     expect(user.password).toEqual("123456");
+  });
+  it('Saves User data to database', (done) => {
+    const user = new User({
+      username: "Test User",
+      email: "example@example.com",
+      password: "123456"
+    })
+
+    user.save((err) => {
+      expect(err).toBeNull();
+
+      User.find((err, users) => {
+        expect(err).toBeNull();
+        expect(users[0].username).toEqual("Test User");
+        expect(users[0].email).toEqual("example@example.com");
+        expect(users[0].password).toEqual("123456");
+
+      });
+      done();
+    });
   });
 })
