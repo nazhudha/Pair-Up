ee5598e727981e03a365cf2e7270f9cf59753275
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-02 17:29:58 +0100

    route from name - skills working. tests passed

--- client/cypress/e2e/signup/user_can_click_on_sign_up_spec.cy.js
+++ client/cypress/e2e/signup/user_can_click_on_sign_up_spec.cy.js
@@ -37,4 +37,37 @@ describe("", () => {
 
     
   });
+  it("User wont progress if either the name fields are empty", () => {
+    
+    cy.visit("/");
+    cy.contains("Sign up").click();
+
+    cy.get(':button').should('be.disabled')
+
+    
+  });
+  it("User wont progress if either the first name field is empty", () => {
+    
+    cy.visit("/");
+    cy.contains("Sign up").click();
+
+    // cy.get("input[name=fname]").type("Paddy")
+    cy.get("input[name=lname]").type("Reynolds")
+
+    cy.get(':button').should('be.disabled')
+
+    
+  });
+  it("User wont progress if either the last name field is empty", () => {
+    
+    cy.visit("/");
+    cy.contains("Sign up").click();
+
+    cy.get("input[name=fname]").type("Paddy")
+    // cy.get("input[name=lname]").type("Reynolds")
+
+    cy.get(':button').should('be.disabled')
+
+    
+  });
 });
--- client/src/App/loginAndSignup/Signup/SignupName/index.js
+++ client/src/App/loginAndSignup/Signup/SignupName/index.js
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {Link} from 'react-router-dom'
 
 export default function SignupName() {
 
-  
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const nextButtonRef = useRef();
 
+    function handleChange() {
+      if ((firstNameRef.current.value.length == 0) || (lastNameRef.current.value.length == 0)) {
+        nextButtonRef.current.disabled = true
+        }
+      else {nextButtonRef.current.disabled = false}
+    }
+  
+ 
   return (
     <>
     <form>
       <label>
         First Name:
-        <input type="text" name="fname" />
+        <input ref={firstNameRef} type="text" name="fname" onChange={handleChange}/>
       </label>
       <label>
         Last Name:
-        <input type="text" name="lname" />
+        <input ref={lastNameRef}type="text" name="lname" onChange={handleChange} />
       </label>
     </form>
     <Link to="/signupSkill">
-      <button>
+      <button ref={nextButtonRef} disabled={true}>
         NEXT
       </button>
     </Link>
