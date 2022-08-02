a696b69204227c5f68a9014e1157510987ebf00b
Christopher <christopher2brown@hotmail.co.uk>
2022-08-02 12:49:43 +0100

    added logout function to sessions'

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 // import modules
-const express = require('express');
+const express = require("express");
 const { json, urlencoded } = express;
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
-const session = require('express-session');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const session = require("express-session");
 
 // app
 const app = express();
@@ -13,6 +13,7 @@ const app = express();
 //sessions for users
 app.use(
   session({
+    key: "user_sid",
     secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
     resave: false,
@@ -30,19 +31,19 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB CONNECTED'))
-  .catch((err) => console.log('DB CONNECTION ERROR', err));
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB CONNECTION ERROR", err));
 
 // middleware
-app.use(morgan('dev')); //logs requests. helps with debugging
+app.use(morgan("dev")); //logs requests. helps with debugging
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cors({ origin: true, credentials: true })); //communicating between front and back end
 
 // routes
-app.use('/user', require('./routes/user'));
-app.use('/sessions', require('./routes/sessions'));
-app.use('/profile', require('./routes/profile'));
+app.use("/user", require("./routes/user"));
+app.use("/sessions", require("./routes/sessions"));
+app.use("/profile", require("./routes/profile"));
 
 // port
 const port = process.env.PORT || 8080; //use PORT set up in enVars, or use 8080
--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -2,11 +2,8 @@ const User = require("../models/user");
 
 const ProfileController = {
   Me: async (req, res) => {
-    //console.log('!!!');
-    console.log(req.session);
     const profile = await User.findOne({ username: req.session.user.username });
     return res.json(profile);
-    //res.render('profile/me');
   },
 
   All: async (req, res) => {
