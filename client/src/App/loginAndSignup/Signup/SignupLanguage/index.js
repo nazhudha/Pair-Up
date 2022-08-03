ab8954b9b3af42957304d11f7e38814e043fe1c8
Christopher <christopher2brown@hotmail.co.uk>
2022-08-03 16:44:06 +0100

    further work to get singuplanuage working

--- client/src/App/loginAndSignup/Signup/SignupLanguage/index.js
+++ client/src/App/loginAndSignup/Signup/SignupLanguage/index.js
@@ -1,22 +1,25 @@
 import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
-export default function SignupLanguage({ addlang, user }) {
+export default function SignupLanguage({ addLang, user }) {
   const rubyRef = useRef(null);
   const javascriptRef = useRef(null);
   const pythonRef = useRef(null);
 
   function sendSkillsArray() {
     let languages = [];
-    console.log(rubyRef.current.checked);
+
     if (rubyRef.current.checked === true) {
       languages.push("ruby");
-    } else if (javascriptRef.current.checked === true) {
-      languages.push("Javascript");
-    } else if (pythonRef.current.checked === true) {
+    }
+    if (javascriptRef.current.checked === true) {
+      languages.push("javascript");
+    }
+    if (pythonRef.current.checked === true) {
       languages.push("python");
     }
-    addlang(languages);
+    console.log(rubyRef.current.checked);
+    addLang(languages);
     console.log(user);
   }
 
@@ -29,17 +32,12 @@ export default function SignupLanguage({ addlang, user }) {
           type="checkbox"
           value="javascript"
           name="javascript"
-        />{" "}
+        />
         javascript
-        <input
-          ref={pythonRef}
-          type="checkbox"
-          value="python"
-          name="python"
-        />{" "}
+        <input ref={pythonRef} type="checkbox" value="python" name="python" />
         python
       </div>
-      <Link to="/signupcredentials" onClick={sendSkillsArray()}>
+      <Link to="/signupcredentials" onClick={sendSkillsArray}>
         <button>NEXT</button>
       </Link>
     </>
