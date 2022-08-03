7cb672e5b8be123ae11b45550d234562be3ed462
Clare Budds <clarebudds@yahoo.co.uk>
2022-08-03 18:22:29 +0100

    Updated font size

--- client/src/App/loginAndSignup/Signup/Welcome/index.js
+++ client/src/App/loginAndSignup/Signup/Welcome/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Welcome() {
   return (
-    <div>Welcome To Pair-Up!</div>
-  )
+    <div>
+      <h1>Welcome to Pair Up!</h1>
+    </div>
+  );
 }
