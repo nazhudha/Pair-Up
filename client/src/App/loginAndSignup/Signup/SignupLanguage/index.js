5bab6303e8b6ba1988b2168538abdfeb0fe9d2dd
Clare Budds <clarebudds@yahoo.co.uk>
2022-08-03 18:14:57 +0100

    styled sign up language page

--- client/src/App/loginAndSignup/Signup/SignupLanguage/index.js
+++ client/src/App/loginAndSignup/Signup/SignupLanguage/index.js
@@ -1,23 +1,18 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignupLanguage() {
-
-
-
-
   return (
     <>
-      <div>
-        <input type="checkbox" value="ruby" name="ruby"/> ruby
-        <input type="checkbox" value="javascript" name="javascript"/> javascript
-        <input type="checkbox" value="python" name="python"/> python
+      <div className="languagetype">
+        <h3>What programming languages do you know?</h3>
+        <input type="radio" value="ruby" name="ruby" /> ruby
+        <input type="radio" value="javascript" name="javascript" /> javascript
+        <input type="radio" value="python" name="python" /> python
       </div>
-    <Link to="/signupcredentials">
-      <button>
-        NEXT
-      </button>
-    </Link>
-  </>
-  )
+      <Link to="/signupcredentials">
+        <button>NEXT</button>
+      </Link>
+    </>
+  );
 }
