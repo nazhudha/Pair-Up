978b3336a27425d82a9b2c9ad2dfe6d01bb1ce60
Clare Budds <clarebudds@yahoo.co.uk>
2022-08-04 15:15:47 +0100

    Created userCards function

--- client/src/App/loginAndSignup/Signup/Welcome/index.js
+++ client/src/App/loginAndSignup/Signup/Welcome/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import UserCards from "./userCards";
 
 export default function Welcome() {
   return (
     <div>
       <h1>Welcome to Pair Up!</h1>
+      <UserCards />
     </div>
   );
 }
---
+++ client/src/App/loginAndSignup/Signup/Welcome/userCards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function UserCards() {
+  return (
+    <div className="cardsContainer">
+      <div className="userCards" style={{ color: "red" }}>
+        Hi there!
+      </div>
+    </div>
+  );
+}
