2b8ed61919fde6b7bb795bd0d311ba502d1260f4
Clare Budds <clarebudds@yahoo.co.uk>
2022-08-04 16:04:56 +0100

    Added images

--- client/src/App/loginAndSignup/Signup/Welcome/userCards.js
+++ client/src/App/loginAndSignup/Signup/Welcome/userCards.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./userCards.css";
+import Chris from "./images/Chris.png";
 
 export default function UserCards() {
   return (
     <div className="cardsContainer">
       <div className="userCards" style={{ color: "red" }}>
-        Hi there!
+        <img src={Chris} alt="Chris" />;<p>Info about the user</p>
+        <button>Profile</button>
+        <button>Contact me</button>
       </div>
     </div>
   );
