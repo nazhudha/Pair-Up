6aa0ffd3dda3bb502808ea56a2f155a107dc115e
shsn1990s <shssn8@gmail.com>
2022-08-05 09:23:15 +0100

    Added test case for user model geocoding functionality

--- backend/spec/model/user.spec.js
+++ backend/spec/model/user.spec.js
@@ -5,32 +5,50 @@ require('../mongodb_helper');
 describe('User model', () => {
   it('create an instance of the User Model Schema', () => {
     const user = new User({
-      username: "Test User",
-      email: "example@example.com",
-      password: "123456"
-    })
-    expect(user.username).toEqual("Test User");
-    expect(user.email).toEqual("example@example.com");
-    expect(user.password).toEqual("123456");
+      username: 'Test User',
+      email: 'example@example.com',
+      password: '123456',
+    });
+    expect(user.username).toEqual('Test User');
+    expect(user.email).toEqual('example@example.com');
+    expect(user.password).toEqual('123456');
   });
   it('Saves User data to database', (done) => {
     const user = new User({
-      username: "Test User",
-      email: "example@example.com",
-      password: "123456"
-    })
+      username: 'Test User',
+      email: 'example@example.com',
+      password: '123456',
+    });
 
     user.save((err) => {
       expect(err).toBeNull();
 
       User.find((err, users) => {
         expect(err).toBeNull();
-        expect(users[0].username).toEqual("Test User");
-        expect(users[0].email).toEqual("example@example.com");
-        expect(users[0].password).toEqual("123456");
-
+        expect(users[0].username).toEqual('Test User');
+        expect(users[0].email).toEqual('example@example.com');
+        expect(users[0].password).toEqual('123456');
       });
       done();
     });
   });
-})
+  it('Saves User data with geocoding data obtained via API', (done) => {
+    const user = new User({
+      username: 'Test User',
+      email: 'example@example.com',
+      password: '123456',
+      postcode: 'E1 6LT',
+    });
+
+    user.save((err) => {
+      expect(err).toBeNull();
+
+      User.find((err, users) => {
+        expect(err).toBeNull();
+        expect(users[0].geoLocation[0]).toEqual(expect.any(Number));
+        expect(users[0].geoLocation[1]).toEqual(expect.any(Number));
+      });
+      done();
+    });
+  });
+});
