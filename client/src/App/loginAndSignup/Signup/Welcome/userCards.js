430d279362413f87bb6d9a2ccd80167724c39aca
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-08-05 12:49:44 +0100

    cards container fixed wit spacing

--- client/src/App/loginAndSignup/Signup/Welcome/userCards.js
+++ client/src/App/loginAndSignup/Signup/Welcome/userCards.js
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 import "./userCards.css";
 import Chris from "./images/Chris.png";
 
-export default function UserCards({user}) {
+export default function UserCards({ user }) {
   return (
-      <div className="userCards">
-        <img src={Chris} alt="Chris" />
-        <div className="userInfo">
-          <p>
-            {user.fname}<br></br>
-            {user.lname}
-            {user.skill}
-            {user.languages}
-          </p>
-        </div>
-        <div className="buttonContainer">
-          <button>Profile</button>
-          <button>Contact me</button>
-        </div>
-      </div>    
+    <div className="userCards">
+      <img src={Chris} alt="Chris" />
+      <div className="userInfo">
+        <p>
+          {`${user.fname} ${user.lname}`}
+          <br></br>
+          {user.skill}
+          <br></br>
+          {user.languages}
+          <br></br>
+        </p>
+      </div>
+      <div className="buttonContainer">
+        <button>Profile</button>
+        <button>Contact me</button>
+      </div>
+    </div>
   );
 }
