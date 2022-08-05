763a1ac46e4bce7cdf91de03288e73fdd3f41c56
Clare Budds <clarebudds@yahoo.co.uk>
2022-08-05 16:59:27 +0100

    navbar with logo

--- client/src/App/loginAndSignup/Signup/Welcome/navbar.js
+++ client/src/App/loginAndSignup/Signup/Welcome/navbar.js
@@ -4,7 +4,7 @@ import Pairuplogo from "./images/Pairuplogo.png";
 export default function Navbar() {
   return (
     <div className="nav-container">
-      <img src={Pairuplogo} alt="logo" />
+      <img src={Pairuplogo} alt="logo" className="nav--icon" />
       <nav></nav>
     </div>
   );
