6d9fa25ed07d5c714d4eca353ec1f93fe22b6b07
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-05 15:23:36 +0100

    new projects page complete

--- client/src/App/Projects/NewProjectPage/index.js
+++ client/src/App/Projects/NewProjectPage/index.js
@@ -1,36 +1,50 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import {useForm} from "react-hook-form"
 import {Link} from 'react-router-dom'
 
 export default function NewProjectPage() {
   const {register, handleSubmit} = useForm();
 
+   const addProjectToDb = async (project) => {
+    try {
+      const res = await fetch("http://localhost:8080/projects/createproject", {
+        method: "POST",
+        body: JSON.stringify(project),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      // const json = await res.json(); // not sure why this line is needed?
+    } catch (err) {}
+  };
+  
+
+
   const onSubmit = (data) => {
-    console.log(data);
+    setProject(data);
+    addProjectToDb(project);
   }
 
+  const [project, setProject] = useState({
+      owner: "", //placeholder id - replace with sessions ID
+      name: "",
+      difficulty: "",
+      expectedProjectLength: "",
+      category: "",
+      preferedSessionLength: "",
+      numberOfAdditionalUsersNeeded: "",
+      summary: "",
+      description: "",
+      users: [], 
+      langWeHave: [],
+      langWeNeed: [],
+      additionalSkillsNeeded: "",
+      displayLocation: false,
+      commitmentLevel: ""
+  });
 
-  // const [project, setProject] = useState({
-  //     owner: "", //placeholder id - replace with sessions ID
-  //     name: "",
-  //     difficulty: "",
-  //     expectedProjectLength: "",
-  //     category: "",
-  //     preferedSessionLength: "",
-  //     numberOfAdditionalUsersNeeded: "",
-  //     summary: "",
-  //     description: "",
-  //     users: [], 
-  //     langWeHave: [],
-  //     langWeNeed: [],
-  //     additionalSkillsNeeded: "",
-  //     displayLocation: false,
-  //     commitmentLevel: ""
-  // });
-  // // function to create project
-  // const addProjectDetails = (owner, name, difficulty, expectedProjectLength) => { //dont pass a function too many arguments
-  //   setProject({ ...user, owner: owner, name: name, difficulty: difficulty, expectedProjectLength: expectedProjectLength});
-  // }
+
   // //function to pull sessions userID to owner and users
   // const addUserIdToProject = () => {
   //   let userID = "62ebab3246601bb290812d9e" //replace with session function
