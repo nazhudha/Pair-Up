4c13e77472d59123e9773faedcb6fbd50b7ef8c8
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-05 11:26:40 +0100

    userCardsContainer done

--- client/src/App/loginAndSignup/Signup/Welcome/index.js
+++ client/src/App/loginAndSignup/Signup/Welcome/index.js
@@ -1,12 +1,46 @@
 import React from "react";
 import UserCards from "./userCards";
-import UserCardsContainer from "./UserCardsContainer";
+import UserCardsContainer from "./AllUserCardsContainer";
 
 export default function Welcome() {
+
+  const usersData = [{
+    fname: "tom",
+    lname: "Reynolds",
+    skill: "Master",
+    languages: ["ruby", "javascript"],
+    username: "paddyr105",
+    email: "paddy@test.com",
+    password: "password",
+    postcode: "se1 1ab"
+  },
+  {
+    fname: "tim",
+    lname: "Reynolds",
+    skill: "Master",
+    languages: ["ruby", "javascript"],
+    username: "paddyr106",
+    email: "paddy@test.com",
+    password: "password",
+    postcode: "se1 1ab"
+  },
+  {
+    fname: "tony",
+    lname: "Reynolds",
+    skill: "Master",
+    languages: ["ruby", "javascript"],
+    username: "paddyr107",
+    email: "paddy@test.com",
+    password: "password",
+    postcode: "se1 1ab"
+  },
+  ]
+  
+
   return (
     <div>
       <h1>Welcome to Pair Up!</h1>
-      <UserCardsContainer />
+      <UserCardsContainer usersData={usersData}/>
     </div>
   );
 }
