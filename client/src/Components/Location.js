3f245585d7afc5e6c7d76a2700c56fa90db935df
shsn1990s <shssn8@gmail.com>
2022-08-07 16:02:35 +0100

    Added profile picture to User Model

--- client/src/Components/Location.js
+++ client/src/Components/Location.js
@@ -1,47 +1,50 @@
 import React, { Component, useEffect, useState } from 'react';
 import L from 'leaflet';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-
 import { Icon } from 'leaflet';
-
 import { motion } from 'framer-motion';
 
 const users = [
   {
-    name: 'Saif',
+    fname: 'Saif',
+    lname: 'Hussain',
     geoLocation: {
-      coordinates: [51.5, 0.12]
+      coordinates: [51.4743, 0.1124]
     },
-    profile: 'https://ca.slack-edge.com/T03ALA7H4-U03BHQMPR8B-ad702ae06c5b-512',
+    picture: 'https://ca.slack-edge.com/T03ALA7H4-U03BHQMPR8B-ad702ae06c5b-512',
     _id: "62ed7d3d4ac6471d1289c40f"
   },
   {
-    name: 'Naz',
+    fname: 'Naz',
+    lname: 'H',
     geoLocation: {
-      coordinates: [51.5074, 0.1272]
+      coordinates: [51.4905, 0.1535]
     },
-    profile: 'https://ca.slack-edge.com/T03ALA7H4-U03BHQNU09Z-4cbaab60cec2-512',
+    picture: 'https://ca.slack-edge.com/T03ALA7H4-U03BHQNU09Z-4cbaab60cec2-512',
   },
   {
-    name: 'Claire',
+    fname: 'Clare',
+    lname: 'Budds',
     geoLocation: {
-      coordinates: [51.4926, 0.1188]
+      coordinates: [51.4806, 0.1284]
     },
-    profile: 'https://ca.slack-edge.com/T03ALA7H4-U03BWD2M7G9-dc8487d8ca75-512',
+    picture: 'https://ca.slack-edge.com/T03ALA7H4-U03BWD2M7G9-dc8487d8ca75-512',
   },
   {
-    name: 'Chris',
+    fname: 'Chris',
+    lname: 'Brown',
     geoLocation: {
-      coordinates: [51.4874, 0.0865]
+      coordinates: [51.48925, 0.110117]
     },
-    profile: 'https://ca.slack-edge.com/T03ALA7H4-U03BL516ULC-a8eb365e0c4c-512',
+    picture: 'https://ca.slack-edge.com/T03ALA7H4-U03BL516ULC-a8eb365e0c4c-512',
   },
   {
-    name: 'Paddy',
+    fname: 'Paddy',
+    lname: 'Reynolds',
     geoLocation: {
-      coordinates: [51.4975, 0.1357]
+      coordinates: [51.4840, 0.1453]
     },
-    profile: 'https://ca.slack-edge.com/T03ALA7H4-U03BHQMHC75-73ac8096e0bb-512',
+    picture: 'https://ca.slack-edge.com/T03ALA7H4-U03BHQMHC75-73ac8096e0bb-512',
   },
 ];
 
@@ -70,7 +73,7 @@ function Location() {
     });
   }, []);
 
-  const position = [51.5, 0.12];
+  const position = [51.4806, 0.1284];
   return (
   //   <div>
   //   <ul>
@@ -80,49 +83,40 @@ function Location() {
   //     })}
   //   </ul>
   // </div>
+  
     <MapContainer
       className="location"
       center={position}
       zoom={14}
       style={{ height: 600, width: 1000 }}
     >
-
+<style>{'body { background-color: white; }'}</style>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        //url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=fsB39cCVxZgXS53pflUQ"
-        //url="https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=fsB39cCVxZgXS53pflUQ"
-        //url="https://api.maptiler.com/maps/voyager/256/{z}/{x}/{y}.png?key=fsB39cCVxZgXS53pflUQ"
         url="https://api.maptiler.com/maps/bright/256/{z}/{x}/{y}.png?key=fsB39cCVxZgXS53pflUQ"
       />
 
       {users.map((location, index) => (
-        
         <div key={index}>
           <Marker
           
             position={location.geoLocation.coordinates}
-            // icon={
-            //   new Icon({
-            //     iconUrl: markerIconPng,
-            //     iconSize: [25, 41],
-            //     iconAnchor: [12, 41],
-            //   })
-            // }
             icon={
               new Icon({
                 
                 className: 'image-icon',
-                iconUrl: `${location.profile}`,              
+                iconUrl: `${location.picture}`,              
                 iconSize: [35, 41],
                 iconAnchor: [12, 41],
               })
             }
-            
           >
             <Popup>
-              <b>{location.name}</b> <br />{' '}
-              <a href={"/profile/" + location._id}>Software Engineer </a>
+              <b>{location.fname} {location.lname}</b> <br />{' '}
+              <br />
+              Software Engineering student<br /> at Makers Academy. <br />{' '}
+              <br />
+              <a href={"/profile/" + location._id}>View Profile </a>
             </Popup>
           </Marker>
         </div>
