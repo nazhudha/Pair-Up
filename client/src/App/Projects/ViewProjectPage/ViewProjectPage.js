25965819fa08d9bef5a915f25e4842fc8d448d46
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-07 17:01:16 +0100

    front-back connection working

--- client/src/App/Projects/ViewProjectPage/ViewProjectPage.js
+++ client/src/App/Projects/ViewProjectPage/ViewProjectPage.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
+import { getOneProjectById } from '../../Functions/getProjects'
 
-
+const dummyProject = "62efabfdd218aebb148b751d"
 
 export default function ViewProjectPage() {
+  const [project, setProject] = useState([]);
+
+
+  const pullMyProject = () => {
+    getOneProjectById(dummyProject) // api function
+      .then((res) => setProject(res))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    pullMyProject();
+  }, []); 
+
+
   return (
-    <div>ViewProjectPage</div>
+    <div>{project.name}</div>
   )
 }
