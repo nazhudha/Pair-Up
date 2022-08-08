f175a128d75ae1ca472637e76a2e4015064ac154
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-08 12:16:20 +0100

    trying somthing new

--- client/src/App/Projects/ViewProjectPage/ViewProjectPage.js
+++ client/src/App/Projects/ViewProjectPage/ViewProjectPage.js
@@ -23,44 +23,49 @@ export default function ViewProjectPage() {
     pullMyProject();
   }, []); 
 
-
-  return (
-    <>
-    {/* banner */}
-      <div>
-        <small>{project.category}</small><br/>
-        <strong>{project.name}</strong><br/>
-        <h4>Difficulty: {project.difficulty} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Commitment: {project.commitmentLevel}&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Session Length: Roughly {project.preferedSessionLength} hour  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Team Size: {project.users.length}/{project.numberOfAdditionalUsersNeeded}</h4> 
-      </div>
-    {/* Summary and Description */}
-      <div>
-        <p>{project.summary}</p>
-        <p>{project.description}</p>
-      </div>   
-    {/* Join this project */}
-      <div>
-        <strong>Join this project</strong>
-        <JoinButton projectId={project._id}/>
-      </div>
-    {/* Skills we have */}
-      <div>
-        <strong>Skills we have </strong><br/>
-        {project.langWeHave.map((languange)=>{
-          return `${languange} `
-        })}
-      </div>
-    {/* Skills we need */}
-      <div>
-        <strong>Skills we need </strong><br/>
-        {project.langWeNeed.map((languange)=>{
-          return `${languange} `
-        })}
-      </div>
-    {/* Users in this project */}
-      <div>
-        <strong>Users in this project </strong><br/>
-        <UserCardsContainer usersData={project.users}/>
-      </div>
-    </>
+console.log(project)
+  return (   
+<>
+{/* banner */}
+  <div>
+    <small>{project.category}</small><br/>
+    <strong>{project.name}</strong><br/>
+    <h4>Difficulty: {project.difficulty} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Commitment: {project.commitmentLevel}&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Session Length: Roughly {project.preferedSessionLength} hour  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Team Size: {project.users.length}/{project.numberOfAdditionalUsersNeeded}</h4> 
+  </div>
+{/* Summary and Description */}
+  <div>
+    <p>{project.summary}</p>
+    <p>{project.description}</p>
+  </div>   
+{/* Join this project */}
+  <div>
+    <strong>Join this project</strong>
+    <JoinButton projectId={project._id}/>
+  </div>
+{/* Skills we have */}
+  <div>
+    <strong>Skills we have </strong><br/>
+    {project.langWeHave.map((languange)=>{
+      return `${languange} `
+    })}
+  </div>
+{/* Skills we need */}
+  <div>
+    <strong>Skills we need </strong><br/>
+    {project.langWeNeed.map((languange)=>{
+      return `${languange} `
+    })}
+  </div>
+{/* Users in this project */}
+  <div>
+    <strong>Users in this project </strong><br/>
+    <UserCardsContainer usersData={project.users}/>
+  </div>
+   {/* Map of users*/}
+   <div>
+    <strong>Where everyone is based</strong><br/>
+    <JoinButton />
+  </div>
+</>
   )
 }
