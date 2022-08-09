840b28da556c455fe11928fcbbba17a09cb69045
Christopher <christopher2brown@hotmail.co.uk>
2022-08-09 15:42:54 +0100

    updated signup names design

--- client/src/App/loginAndSignup/Signup/SignupName/index.js
+++ client/src/App/loginAndSignup/Signup/SignupName/index.js
@@ -7,6 +7,20 @@ export default function SignupName({ addName, user }) {
   const firstNameRef = useRef();
   const lastNameRef = useRef();
   const nextButtonRef = useRef();
+  const firstName = {
+    minWidth: 150,
+    minHeight: 100,
+    bottom: -300,
+    left: 600,
+    fontSize: 20,
+  };
+  const lastName = {
+    minWidth: 150,
+    minHeight: 100,
+    bottom: -300,
+    left: 700,
+    fontSize: 20,
+  };
 
   function handleChange() {
     if (
@@ -20,40 +34,69 @@ export default function SignupName({ addName, user }) {
   }
 
   return (
-    <>
-      <div className="form-container">
-        <div className="signup-wrapper">
-          <form>
-            First Name:
-            <input
-              ref={firstNameRef}
-              type="text"
-              name="fname"
-              onChange={handleChange}
-            />
-            <label>
-              Last Name:
-              <input
-                ref={lastNameRef}
-                type="text"
-                name="lname"
-                onChange={handleChange}
-              />
-            </label>
-          </form>
+    <div>
+      <TextField
+        type="text"
+        inputRef={firstNameRef}
+        style={firstName}
+        id="filled-basic"
+        label="First Name"
+        variant="filled"
+      />
+      <TextField
+        type="text"
+        inputRef={lastNameRef}
+        style={lastName}
+        id="filled-basic"
+        label="Last Name"
+        variant="filled"
+      />
+      <Link
+        to="/signupskill"
+        onClick={() => {
+          addName(firstNameRef.current.value, lastNameRef.current.value);
+        }}
+      >
+        <button className="next-btn" ref={nextButtonRef}>
+          NEXT
+        </button>
+      </Link>
+    </div>
 
-          <Link
-            to="/signupskill"
-            onClick={() => {
-              addName(firstNameRef.current.value, lastNameRef.current.value);
-            }}
-          >
-            <button className="next-btn" ref={nextButtonRef} disabled={true}>
-              NEXT
-            </button>
-          </Link>
-        </div>
-      </div>
-    </>
+    // <>
+    //   <div className="form-container">
+    //     <div className="signup-wrapper">
+    //       <form>
+    //         First Name:
+    //         <input
+    //           ref={firstNameRef}
+    //           type="text"
+    //           name="fname"
+    //           onChange={handleChange}
+    //         />
+    //         <label>
+    //           Last Name:
+    //           <input
+    //             ref={lastNameRef}
+    //             type="text"
+    //             name="lname"
+    //             onChange={handleChange}
+    //           />
+    //         </label>
+    //       </form>
+
+    //       <Link
+    //         to="/signupskill"
+    //         onClick={() => {
+    //           addName(firstNameRef.current.value, lastNameRef.current.value);
+    //         }}
+    //       >
+    //         <button className="next-btn" ref={nextButtonRef} disabled={true}>
+    //           NEXT
+    //         </button>
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </>
   );
 }
