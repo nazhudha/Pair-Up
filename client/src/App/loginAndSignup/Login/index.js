8bee34f166f4bd159eb3a91e10db687c6cf324c4
shsn1990s <shssn8@gmail.com>
2022-08-09 14:52:17 +0100

    Further changes to login page

--- client/src/App/loginAndSignup/Login/index.js
+++ client/src/App/loginAndSignup/Login/index.js
@@ -1,8 +1,15 @@
 import React, { Component, useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
+import LockIcon from '@mui/icons-material/Lock';
+import Box from '@mui/material/Box';
+
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -46,58 +53,74 @@ const Login = () => {
   const paperStyle = {
     padding: 20,
     height: '70vh',
-    width: 280,
+    width: 500,
     margin: '20px auto',
   };
-  const avatarStyle = { backgroundColor: '#1bbd7e' };
+  const avatarStyle = { backgroundColor: '#89CFF0' };
   const btnstyle = { margin: '8px 0' };
+  const fontColour = { color: 'black', align: 'left' };
+  //const passwordStyle = { minWidth: 150, minHeight: 100, bottom: 0 };
   return (
-    <Paper elevation={10} style={paperStyle}>
-      <form onSubmit={submit}>
-        <h3>Login</h3>
-        <div className="form-group">
-          {/* <label>Email</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center" spacing={5}>
+          <Avatar style={avatarStyle}>
+            <LockIcon />
+          </Avatar>
+        </Grid>
+        <br />
+        <Typography variant="h3" gutterBottom component="div">
+          Sign In
+        </Typography>
+        <form onSubmit={submit}>
+          <div className="form-group"></div>
+          {/* <TextField label="Email" placeholder="Enter Email:" fullWidth />
+          <TextField label="Password" placeholder="Enter Password:" fullWidth /> */}
+          <TextField
+            // style={passwordStyle}
+            minWidth={150}
+            id="standard-basic"
+            fullWidth
+            label="Email"
+            placeholder="Enter Email:"
+            variant="standard"
+            required
             onChange={(e) => setEmail(e.target.value)}
-          /> */}
-        </div>
-        <TextField
-          label="Email"
-          placeholder="Enter Email:"
-          fullWidth
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          label="Password"
-          placeholder="Enter Password:"
-          fullWidth
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className="form-group">
-          {/* <label>Password</label>
-          <input
+          />
+          <TextField
+            // style={passwordStyle}
+            label="Password"
+            id="standard-basic"
+            fullWidth
             type="password"
-            className="form-control"
-            placeholder="Password"
+            placeholder="Enter Password:"
+            variant="standard"
+            required
             onChange={(e) => setPassword(e.target.value)}
-          /> */}
-
-          <Button
-            type="submit"
-            variant="contained"
-            onClick={() => '/profile'}
-            className="btn btn-primary btn-bloack"
-          >
-            Login
-          </Button>
-        </div>
-      </form>
-    </Paper>
+          />
+          <div className="form-group">
+            <Box textAlign="center" m={2} pt={3}>
+              <Button
+                type="submit"
+                variant="contained"
+                onClick={() => '/profile'}
+                className="btn btn-primary btn-bloack"
+              >
+                Login
+              </Button>
+            </Box>
+            <Typography>
+              <Link href="#">Forgot password ?</Link>
+            </Typography>
+            <Typography>
+              {' '}
+              Don't have an account?
+              <Link href="/Signupname"> Sign Up</Link>
+            </Typography>
+          </div>
+        </form>
+      </Paper>
+    </Grid>
   );
 };
 
