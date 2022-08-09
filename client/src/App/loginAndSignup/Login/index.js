f9953848b476bbb22d34d73e06353080fe889f9d
shsn1990s <shssn8@gmail.com>
2022-08-09 12:18:59 +0100

    Login Page Textfields

--- client/src/App/loginAndSignup/Login/index.js
+++ client/src/App/loginAndSignup/Login/index.js
@@ -1,10 +1,12 @@
-import React, { Component, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
-import Button from "@mui/material/Button";
+import React, { Component, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
 
   const submit = async (e) => {
@@ -14,16 +16,16 @@ const Login = () => {
       password: password,
     };
     const requestOptions = {
-      method: "POST",
-      credentials: "include",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email: email, password: password }),
     };
-    fetch("http://localhost:8080/auth", requestOptions)
+    fetch('http://localhost:8080/auth', requestOptions)
       .then((response) => response.json())
       .then((data) => {
         console.log(data.token);
-        localStorage.setItem("token", data.token);
+        localStorage.setItem('token', data.token);
       });
     setRedirect(true);
   };
@@ -41,39 +43,61 @@ const Login = () => {
   //   }
   //   createSignInObject(emailRef.current.value, passwordRef.current.value);
   // }
-
+  const paperStyle = {
+    padding: 20,
+    height: '70vh',
+    width: 280,
+    margin: '20px auto',
+  };
+  const avatarStyle = { backgroundColor: '#1bbd7e' };
+  const btnstyle = { margin: '8px 0' };
   return (
-    <form onSubmit={submit}>
-      <h3>Login</h3>
-      <div className="form-group">
-        <label>Email</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Email"
+    <Paper elevation={10} style={paperStyle}>
+      <form onSubmit={submit}>
+        <h3>Login</h3>
+        <div className="form-group">
+          {/* <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          /> */}
+        </div>
+        <TextField
+          label="Email"
+          placeholder="Enter Email:"
+          fullWidth
+          required
           onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Password"
+        <TextField
+          label="Password"
+          placeholder="Enter Password:"
+          fullWidth
+          required
           onChange={(e) => setPassword(e.target.value)}
         />
+        <div className="form-group">
+          {/* <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          /> */}
 
-        <Button
-          type="submit"
-          variant="contained"
-          onClick={() => "/profile"}
-          className="btn btn-primary btn-bloack"
-        >
-          Login
-        </Button>
-      </div>
-    </form>
+          <Button
+            type="submit"
+            variant="contained"
+            onClick={() => '/profile'}
+            className="btn btn-primary btn-bloack"
+          >
+            Login
+          </Button>
+        </div>
+      </form>
+    </Paper>
   );
 };
 
