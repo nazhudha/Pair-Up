0077ac9ecb0978e1e09c4a159155e2c13388a6cc
Clare Budds <clarebudds@yahoo.co.uk>
2022-08-10 16:15:23 +0100

    centred buttons

--- client/src/App/Welcome/NewUserCard.js
+++ client/src/App/Welcome/NewUserCard.js
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import profilePic from "./images/profile-pic-stock.webp"; //test
+import Grid from "@material-ui/core/Grid";
 
-
-export default function NewUserCard({user}) {
+export default function NewUserCard({ user }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -19,16 +19,22 @@ export default function NewUserCard({user}) {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-        {`${user.fname} ${user.lname}`}
+          {`${user.fname} ${user.lname}`}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-        {`${user.fname} is a ${user.skill} coder who knows ${user.languages.length} languages.`}
+          {`${user.fname} is a ${user.skill} coder who knows ${user.languages.length} languages.`}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Profile</Button>
-        <Button size="small">Pair now</Button>
+        <Grid container justify="center">
+          <Button variant="contained" size="small">
+            Profile
+          </Button>
+          <Button variant="contained" size="small">
+            Pair now
+          </Button>
+        </Grid>
       </CardActions>
-      </Card>
+    </Card>
   );
 }
