a2ae1ea7cf2867ae03817fb2c77f57f1ded5668a
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-10 16:32:29 +0100

    test3

--- client/src/App/Welcome/index.js
+++ client/src/App/Welcome/index.js
@@ -7,10 +7,17 @@ import { getAllUsers, getAllUsersById } from "../Functions/getAllUsers";
 
 export default function Welcome() {
   const [usersData, setUsersData] = useState([]);
+  const [friendsData, setFriendsData] = useState([]);
 
   const pullUsers = () => {
     getAllUsers() // api function
       .then((res) => setUsersData(res))
+      .catch((err) => console.log(err));
+  };
+
+  const getAllUsersById = () => {
+    getAllUsers() // api function
+      .then((res) => setFriendsData(res))
       .catch((err) => console.log(err));
   };
 
@@ -21,6 +28,10 @@ export default function Welcome() {
   return (
     <div>
       <h1>Welcome to Pair Up!</h1>
+      <div className="cardsContainer">
+        <UserCardsContainer usersData={friendsData} />
+      </div>
+
       <div className="cardsContainer">
         <UserCardsContainer usersData={usersData} />
       </div>
