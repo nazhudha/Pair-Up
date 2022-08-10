f059a2c715e71c5ec574617e3f6282202c429d5b
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-10 15:54:40 +0100

    small fix

--- client/src/App/Welcome/AllUserCardsContainer.js
+++ client/src/App/Welcome/AllUserCardsContainer.js
@@ -6,18 +6,13 @@ import NewUserCard from './NewUserCard';
 
 
 export default function UserCardsContainer({usersData}) {
-  let i = 0
   return usersData.map((userData) => {
     //render a todo with props
-    while (i <= 3) {
     return (
       
       // <UserCards user={userData} key={userData.username}/>
       < NewUserCard user={userData} key={userData.username}/>
     );
-    }
-    i++
-    console.log(i)
   });
 
 }
