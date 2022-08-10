612edbeb033640af89fe7d5f9f16f6535f2b7fc2
Christopher <christopher2brown@hotmail.co.uk>
2022-08-10 15:19:56 +0100

    make it better

--- client/src/App/Projects/NewProjectPage/index.js
+++ client/src/App/Projects/NewProjectPage/index.js
@@ -1,17 +1,17 @@
-import React, {useRef, useState} from 'react'
-import {useForm} from "react-hook-form"
-import {Link} from 'react-router-dom'
+import React, { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 export default function NewProjectPage() {
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
 
-   const addProjectToDb = async (createdProject) => {
-    console.log(JSON.stringify(createdProject))
+  const addProjectToDb = async (createdProject) => {
+    console.log(JSON.stringify(createdProject));
     let config = null;
-    if (localStorage.getItem('token') !== null) {
+    if (localStorage.getItem("token") !== null) {
       config = {
         headers: {
-          'x-auth-token': localStorage.getItem('token'),
+          "x-auth-token": localStorage.getItem("token"),
           "Content-Type": "application/json",
         },
         method: "POST",
@@ -20,21 +20,21 @@ export default function NewProjectPage() {
     }
 
     try {
-      const res = await fetch("http://localhost:8080/projects/createproject", config
-      ).then(res => {
-        console.log(res)
+      const res = await fetch(
+        "http://localhost:8080/projects/createproject",
+        config
+      ).then((res) => {
+        console.log(res);
         window.location = "http://localhost:3000/project/home";
-
       });
-  
+
       // const json = await res.json(); // not sure why this line is needed?
     } catch (err) {}
   };
-  
 
   const submitForm = (data) => {
     addProjectToDb(data);
-  }
+  };
 
   // //function to pull sessions userID to owner and users
   // const addUserIdToProject = () => {
@@ -44,234 +44,298 @@ export default function NewProjectPage() {
 
   return (
     <>
-    <form onSubmit={handleSubmit(submitForm)}>
-
-      <label>
-        Name
-        <input type="text" name="name" {...register('name', { required: true })}/>
-      </label>
-
-      <label>
-        Category
-        <input type="text" name="category" {...register('category', { required: true })}/>
-      </label>
-
-      <h3>Describe briefly what you are trying to build</h3>
-      <textarea name="summary" {...register('summary', { required: true })}>
-      </textarea>
-      
-
-      <h3>How long do you think this project will take?</h3>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            {...register("name", { required: true })}
+          />
+        </label>
+        <label>
+          Category
+          <input
+            type="text"
+            name="category"
+            {...register("category", { required: true })}
+          />
+        </label>
+        <h3>Describe briefly what you are trying to build</h3>
+        <textarea
+          name="summary"
+          {...register("summary", { required: true })}
+        ></textarea>
+        <h3>How long do you think this project will take?</h3>
         <input
           type="radio"
           name="expectedProjectLength"
           value="<1 day"
-          {...register('expectedProjectLength', { required: true })}
+          {...register("expectedProjectLength", { required: true })}
         />
         &lt;1 day
         <input
           type="radio"
           name="expectedProjectLength"
           value=">1 day"
-          {...register('expectedProjectLength', { required: true })}
+          {...register("expectedProjectLength", { required: true })}
         />
         &gt;1 day
         <input
           type="radio"
           name="expectedProjectLength"
           value="2-3 days"
-          {...register('expectedProjectLength', { required: true })}
+          {...register("expectedProjectLength", { required: true })}
         />
         2-3 days
         <input
           type="radio"
           name="expectedProjectLength"
           value="<1 week"
-          {...register('expectedProjectLength', { required: true })}
+          {...register("expectedProjectLength", { required: true })}
         />
         &lt;1 week
         <input
           type="radio"
           name="expectedProjectLength"
           value=">1 week"
-          {...register('expectedProjectLength', { required: true })}
+          {...register("expectedProjectLength", { required: true })}
         />
         &gt;1 week
         <input
           type="radio"
           name="expectedProjectLength"
           value="2-3 weeks"
-          {...register('expectedProjectLength', { required: true })}
+          {...register("expectedProjectLength", { required: true })}
         />
         2-3 weeks
         <input
           type="radio"
           name="expectedProjectLength"
           value="Longer"
-          {...register('expectedProjectLength', { required: true })}
+          {...register("expectedProjectLength", { required: true })}
         />
         Longer
-    
-     <h3>How much of a commitment are you looking for from your team?</h3>
-      <input
-        type="radio"
-        name="commitmentLevel"
-        value="eh, whenever is good for them!"
-        {...register('commitmentLevel', { required: true })}
-      />
-      eh, whenever is good for them!
-      <input
-        type="radio"
-        name="commitmentLevel"
-        value="maybe 1 session a week?"
-        {...register('commitmentLevel', { required: true })}
-      />
-      maybe 1 session a week?
-      <input
-        type="radio"
-        name="commitmentLevel"
-        value="couple times a week preferably"
-        {...register('commitmentLevel', { required: true })}
-      />
-      couple times a week preferably
-
- 
-     
-      <h3>How difficult do you think this project will be?</h3>
-      <input
-        type="radio"
-        name="difficulty"
-        value="Very Easy"
-        {...register('difficulty', { required: true })}
-      />
-      Quite easy. A good beginner project for new coders just starting out.
-      <input
-        type="radio"
-        name="difficulty"
-        value="Easy"
-        {...register('difficulty', { required: true })}
-      />
-      Not too tricky. But might be a challenge for absolute beginner.
-      <input
-        type="radio"
-        name="difficulty"
-        value="Intermediate"
-        {...register('difficulty', { required: true })}
-      />
-      It will likely need people with a good understanding of their perfered language, who have built apps and websites previously.
-      <input
-        type="radio"
-        name="difficulty"
-        value="Hard"
-        {...register('difficulty', { required: true })}
-      />
-      It will likely need people who have worked as coders before.
-      <input
-        type="radio"
-        name="difficulty"
-        value="Very Hard"
-        {...register('difficulty', { required: true })}
-      />
-      It need very experienced professional coders.
-      <input
-        type="radio"
-        name="difficulty"
-        value="Master"
-        {...register('difficulty', { required: true })}
-      />
-      It will require people looking to tackle the most severe problem solving.
-
-      <h3>How long do you want a pair-up session to be?</h3>
-      <label>
-      <input type="number" name="preferedSessionLength" min="1" max="8" {...register('preferedSessionLength', { required: true })}/>
-      (1 - 8 hours)
-      </label>
-
-      <h3>Write the full details of what you want to achieve in this project:</h3>
-      <textarea name="description" {...register('description', { required: true })}>
-      </textarea>
-    
-      <br></br>
-      <label>
-      display your location?
-      <input type="checkbox" name="displayLocation" value={true} {...register('displayLocation')}/>
-      </label>
-
-      <h3>How many additional people are you looking for to help you in this project</h3>
-      <label>
-      <input type="number" name="numberOfAdditionalUsersNeeded" min="1" max="5" {...register('numberOfAdditionalUsersNeeded', { required: true })}/>
-      (max 5)
-      </label>
-
-
-      <h3>What skills are you looking for to help you on this project? </h3>
+        <h3>How much of a commitment are you looking for from your team?</h3>
+        <input
+          type="radio"
+          name="commitmentLevel"
+          value="eh, whenever is good for them!"
+          {...register("commitmentLevel", { required: true })}
+        />
+        eh, whenever is good for them!
+        <input
+          type="radio"
+          name="commitmentLevel"
+          value="maybe 1 session a week?"
+          {...register("commitmentLevel", { required: true })}
+        />
+        maybe 1 session a week?
+        <input
+          type="radio"
+          name="commitmentLevel"
+          value="couple times a week preferably"
+          {...register("commitmentLevel", { required: true })}
+        />
+        couple times a week preferably
+        <h3>How difficult do you think this project will be?</h3>
+        <input
+          type="radio"
+          name="difficulty"
+          value="Very Easy"
+          {...register("difficulty", { required: true })}
+        />
+        Quite easy. A good beginner project for new coders just starting out.
+        <input
+          type="radio"
+          name="difficulty"
+          value="Easy"
+          {...register("difficulty", { required: true })}
+        />
+        Not too tricky. But might be a challenge for absolute beginner.
+        <input
+          type="radio"
+          name="difficulty"
+          value="Intermediate"
+          {...register("difficulty", { required: true })}
+        />
+        It will likely need people with a good understanding of their perfered
+        language, who have built apps and websites previously.
+        <input
+          type="radio"
+          name="difficulty"
+          value="Hard"
+          {...register("difficulty", { required: true })}
+        />
+        It will likely need people who have worked as coders before.
+        <input
+          type="radio"
+          name="difficulty"
+          value="Very Hard"
+          {...register("difficulty", { required: true })}
+        />
+        It need very experienced professional coders.
+        <input
+          type="radio"
+          name="difficulty"
+          value="Master"
+          {...register("difficulty", { required: true })}
+        />
+        It will require people looking to tackle the most severe problem
+        solving.
+        <h3>How long do you want a pair-up session to be?</h3>
+        <label>
+          <input
+            type="number"
+            name="preferedSessionLength"
+            min="1"
+            max="8"
+            {...register("preferedSessionLength", { required: true })}
+          />
+          (1 - 8 hours)
+        </label>
+        <h3>
+          Write the full details of what you want to achieve in this project:
+        </h3>
+        <textarea
+          name="description"
+          {...register("description", { required: true })}
+        ></textarea>
+        <br></br>
+        <label>
+          display your location?
+          <input
+            type="checkbox"
+            name="displayLocation"
+            value={true}
+            {...register("displayLocation")}
+          />
+        </label>
+        <h3>
+          How many additional people are you looking for to help you in this
+          project
+        </h3>
+        <label>
+          <input
+            type="number"
+            name="numberOfAdditionalUsersNeeded"
+            min="1"
+            max="5"
+            {...register("numberOfAdditionalUsersNeeded", { required: true })}
+          />
+          (max 5)
+        </label>
+        <h3>What skills are you looking for to help you on this project? </h3>
         {/* <i className="devicon-coffeescript-original" name="langWeNeed" value="coffeescript" type="checkbox" onClick={()=>{console.log("hello")}} {...register('langWeNeed')}></i> */}
-       <div>
-       <i className="devicon-bootstrap-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="bootstrap" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-ruby-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="ruby" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-javascript-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="javascript" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-python-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="python" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-java-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="java" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-cplusplus-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="cplusplus" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-typescript-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="typescript" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-csharp-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="csharp" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-docker-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="docker" {...register('langWeNeed')}/>
-      </div>
-      <div>
-      <i className="devicon-django-plain"></i>
-        <br></br>
-        <input type="checkbox" name="langWeNeed" value="django" {...register('langWeNeed')}/>
-      </div>
-      
-      
-      
-
-
+        <div>
+          <i className="devicon-bootstrap-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="bootstrap"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-ruby-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="ruby"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-javascript-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="javascript"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-python-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="python"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-java-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="java"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-cplusplus-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="cplusplus"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-typescript-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="typescript"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-csharp-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="csharp"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-docker-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="docker"
+            {...register("langWeNeed")}
+          />
+        </div>
+        <div>
+          <i className="devicon-django-plain"></i>
+          <br></br>
+          <input
+            type="checkbox"
+            name="langWeNeed"
+            value="django"
+            {...register("langWeNeed")}
+          />
+        </div>
         {/* add more icons from https://devicon.dev/ */}
         <br></br>
-
-        <textarea name="description" placeholder="other details" {...register('additionalSkillsNeeded')}/>
-
+        <textarea
+          name="description"
+          placeholder="other details"
+          {...register("additionalSkillsNeeded")}
+        />
         <input type="submit" value="submit" />
       </form>
-
-  
-      
-    
-  </>
-  )
+    </>
+  );
 }
