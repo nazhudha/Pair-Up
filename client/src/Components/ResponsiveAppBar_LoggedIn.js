72f38a808e3e2e36de489754d4995ce006571489
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-08-10 16:23:46 +0100

    added find user

--- client/src/Components/ResponsiveAppBar_LoggedIn.js
+++ client/src/Components/ResponsiveAppBar_LoggedIn.js
@@ -13,8 +13,8 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Home', 'Profile', 'My Projects', 'New Projects'];
-const pageLink = ['/welcome', '/profile', '/project/home', '/project/newproject'];
+const pages = ['Profile', 'My Projects', 'New Projects', 'View Projects', 'Find User'];
+const pageLink = ['/profile', '/project/home', '/project/newproject', '/project/view/:id', '/find'];
 
 const settings = ['Logout'];
 
