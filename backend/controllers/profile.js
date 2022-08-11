6138471c8fabe82fa5a70608dcbb9e6800f2867d
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-08-11 12:40:40 +0100

    friend button functionality working

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -27,37 +27,37 @@ const ProfileController = {
   AddFriend: async (req, res) => {
     console.log(req.user.id)
     console.log(req.params)
-    // const friend = await User.findById(req.body._id)
-    // await User.updateOne(
-    //   {
-    //     _id: req.params.id,
+    const friend = await User.findById(req.user.id)
+    await User.updateOne(
+      {
+        _id: req.params.id,
      
-    //   },
-    //   {
-    //     $addToSet: { friends: req.body._id},
+      },
+      {
+        $addToSet: { friends: req.user.id},
 
   
         
-    //   },
-    //   {
-    //     upsert: true,
-    //     runValidators: true
-    //   });
-    //   await User.updateOne(
-    //     {
-    //       _id: req.body._id,
+      },
+      {
+        upsert: true,
+        runValidators: true
+      });
+      await User.updateOne(
+        {
+          _id: req.user.id,
        
-    //     },
-    //     {
-    //       $addToSet: { friends: req.params.id},
+        },
+        {
+          $addToSet: { friends: req.params.id},
   
     
           
-    //     },
-    //     {
-    //       upsert: true,
-    //       runValidators: true
-    //     });
+        },
+        {
+          upsert: true,
+          runValidators: true
+        });
     
       res.status(201).send("joined the project!")
   },
